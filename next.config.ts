3d4f2b80e3811a793d284a77a7e6fe5cbd304e78
MilanPraz <mln.praz12@gmail.com>
2024-11-24 22:34:57 +0545

    github workflow fix11

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // output: "export",
-  output: "export", // This enables static export
-  distDir: "out", // Optional: ensure that 'out' is used as the output directory
+  // output: "export", // This enables static export
+  // distDir: "out", // Optional: ensure that 'out' is used as the output directory
 };
 
 export default nextConfig;
